1d5cf8af45c577018d7cac693b69899a399d9da9
Vinicius Isola <viniciusisola@gmail.com>
2023-08-12 19:16:02 -0400

    Refactor score and other things out of level

--- Game/Game.gd
+++ Game/Game.gd
@@ -1,16 +1,58 @@
 extends Node2D
 
+var paused = false
+var score = 0
+
+func _on_BlockContainer_block_died(blocks_left):
+	score += 1
+	$Score.text = str(score)
+	if blocks_left == 0:
+		$Win.play()
+		$Level.clear()
+		start()
+
 func _ready():
-	start()
+	go_to_inital_menu()
+	
+func _process(delta):
+	if Input.is_action_just_released("ui_select"):
+		if !$Level.is_running():
+			return
+
+		if paused:
+			unpause()
+		else:
+			pause()
+
+func _on_Level_game_started():
+	$PressSpaceToStart.hide()
 
 func _on_InitialMenu_start_game():
-	$InitialMenu.hide()
-	$Level.show()
-	$Level.start()
-
-func _on_Level_game_over():
 	start()
 
-func start():
+func _on_Level_game_over():
+	go_to_inital_menu()
+
+func go_to_inital_menu():
 	$InitialMenu.show()
+	$PressSpaceToStart.hide()
 	$Level.hide()
+	$Pause.hide()
+	$Score.hide()
+
+func pause():
+	paused = true
+	$Level.pause()
+	$Pause.show()
+	
+func start():
+	$InitialMenu.hide()
+	$PressSpaceToStart.show()
+	$Level.show()
+	$Level.start()
+	$Score.show()
+
+func unpause():
+	paused = false
+	$Level.unpause()
+	$Pause.hide()
--- Level/Level.gd
+++ Level/Level.gd
@@ -1,30 +1,19 @@
 extends Node2D
 
 signal game_over
-signal blocks_changed
+signal game_started
 
 var Ball = preload('res://Ball/Ball.tscn')
 
 var ball_instance
 
-var score = 0
-
-func _on_BlockContainer_block_died(blocks_left):
-	score += 1
-	$Score.text = str(score)
-	emit_signal("blocks_changed", blocks_left,  $BlockContainer.blocks.size())
-	if blocks_left == 0:
-		$Win.play()
-		clear()
-		start()
+var running = false
 
 func _on_Ball_reached_bottom():
 	game_over()
 
 func _process(delta):
 	if Input.is_action_just_released("ui_select"):
-		$PressSpaceToStart.visible = false
-		$Score.visible = true
 		if !ball_instance:
 			ball_instance = Ball.instance()
 			add_child(ball_instance)
@@ -32,11 +21,8 @@ func _process(delta):
 			ball_instance.position.y -= 50
 			ball_instance.reset_speed()
 			ball_instance.connect("reached_bottom", self, "_on_Ball_reached_bottom")
-		else:
-			if $Pause.visible:
-				unpause()
-			else:
-				pause()
+			emit_signal("game_started")
+			running = true
 
 func clear():
 	ball_instance.queue_free()
@@ -44,25 +30,23 @@ func clear():
 
 func game_over():
 	clear()
+	running = false
 	emit_signal("game_over")
+
+func is_running():
+	return running
 	
 func pause():
-	$Pause.visible = true
 	$Player.stop()
 	ball_instance.pause()
 	
 func start():
-	$PressSpaceToStart.visible = true
 	$Player.visible = true
-	$Score.visible = false
-	$Pause.visible = false
-	
 	$Player.start()
 	
 	$BlockContainer.clear()
 	$BlockContainer.generate_blocks()
 
 func unpause():
-	$Pause.visible = false
 	$Player.start()
 	ball_instance.unpause()
