bdaf0c5834a0a84b5014f13318a3079b1ae71356
Vinicius Isola <viniciusisola@gmail.com>
2023-08-19 13:53:55 -0400

    Fix bug pressing space before game is ready

--- Level/Level.gd
+++ Level/Level.gd
@@ -8,17 +8,22 @@ var Ball = preload('res://Ball/Ball.tscn')
 
 var ball_instance
 
+var ready_to_start = false
 var running = false
-var level = 1
+var initial_level = 1
+var level = initial_level
 
 func _on_BlockContainer_block_died(blocks_left):
 	emit_signal("block_died", blocks_left)
 
 func _on_Ball_reached_bottom():
-	level = 1
+	level = initial_level
 	game_over()
 
 func _process(delta):
+	if !ready_to_start:
+		return
+
 	if Input.is_action_just_released("ui_select"):
 		if !ball_instance:
 			ball_instance = Ball.instance()
@@ -35,6 +40,7 @@ func clear():
 	ball_instance = null
 
 func game_over():
+	ready_to_start = false
 	clear()
 	running = false
 	emit_signal("game_over")
@@ -58,6 +64,8 @@ func start():
 	
 	$BlockContainer.clear()
 	$BlockContainer.generate_blocks(level)
+	
+	ready_to_start = true
 
 func unpause():
 	$Player.start()
