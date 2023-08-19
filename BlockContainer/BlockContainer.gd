107a8510a28564dc20f3e76a7fd6454a074548fa
Vinicius Isola <viniciusisola@gmail.com>
2023-08-19 09:41:07 -0400

    Adjust block generation algorithm to fit space perfectly

--- BlockContainer/BlockContainer.gd
+++ BlockContainer/BlockContainer.gd
@@ -4,10 +4,10 @@ var Block = preload('res://Block/Block.tscn')
 
 signal block_died
 
-var margin_x = 20
-var margin_y = 10
-var padding_x = 10
-var padding_y = 10
+var margin_x = 25
+var margin_y = 25
+var desired_padding_x = 15
+var desired_padding_y = 15
 var max_blocks_y = 16
 
 var blocks = []
@@ -27,19 +27,35 @@ func clear():
 func generate_blocks(level):
 	var container_size = $ReferenceRect.get_rect().size
 	var block_size = Vector2(80, 23)
+	
+	var size_x = container_size.x - 2 * margin_x
+	var block_count_x = floor(size_x / block_size.x)
+	
+	var total_size_x = desired_padding_x * (block_count_x - 1) + 2 * margin_x + block_count_x * block_size.x
+	while total_size_x > container_size.x:
+		block_count_x -= 1
+		total_size_x = desired_padding_x * (block_count_x - 1) + 2 * margin_x + block_count_x * block_size.x
 
-	var block_count_x = ceil((container_size.x - 2 * margin_x) / block_size.x - 1)
-	var block_count_y = level
+	var space_left_x = container_size.x - 2 * margin_x - block_count_x * block_size.x
+	var this_padding_x = floor(space_left_x / (block_count_x - 1))
+
+	var block_count_y = 3 * level
 	if block_count_y > max_blocks_y:
 		block_count_y = max_blocks_y
 
 	for i in block_count_x:
 		for j in block_count_y:
-			print(j, block_count_y)
+
+			var extraDurability = j - level
+			if extraDurability < 0:
+				extraDurability = 0
+
 			var block_instance = Block.instance()
-			block_instance.durability = j + 1
+			block_instance.durability = extraDurability + 1
+
 			blocks.push_back(block_instance)
 			$ReferenceRect.add_child(block_instance)
 			block_instance.connect("died", self, "_on_Block_died")
-			block_instance.position.x = i * (block_size.x + padding_x) + padding_x
-			block_instance.position.y = j * (block_size.y + padding_y) + padding_y
+
+			block_instance.position.x = margin_x + i * (block_size.x + this_padding_x)
+			block_instance.position.y = j * (block_size.y + desired_padding_y) + desired_padding_y
